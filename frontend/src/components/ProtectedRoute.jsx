7d579c69cca217671905c80e80ab3dacdbbbe8fd
Harsh Bhardwaj <89222581+Harshsfd@users.noreply.github.com>
2025-09-05 16:42:57 +0530

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,9 +1,11 @@
-import React, { useContext } from "react";
-import { Navigate } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
-export default function ProtectedRoute({ children }) {
-  const { user } = useContext(AuthContext);
-  if (!user) return <Navigate to="/login" />;
+const ProtectedRoute = ({ children }) => {
+  const { token } = useContext(AuthContext);
+  if (!token) return <Navigate to="/" />;
   return children;
-}
+};
+
+export default ProtectedRoute;
