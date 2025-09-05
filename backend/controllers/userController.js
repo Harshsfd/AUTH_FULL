f7d922297c5b577030c4a89d93318edc2b2ff647
Harsh Bhardwaj <89222581+Harshsfd@users.noreply.github.com>
2025-09-05 16:36:58 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,6 @@
-/**
- * GET /api/users/profile
- * Protected route (authMiddleware)
- */
 export const getProfile = async (req, res) => {
   try {
-    const user = req.user; // set in authMiddleware
+    const user = req.user;
     res.json({ id: user._id, name: user.name, email: user.email, mobile: user.mobile, createdAt: user.createdAt });
   } catch (err) {
     console.error('PROFILE ERROR:', err);
