2efc7a739396867f2ad58012b10b2ae1933f292d
Harsh Bhardwaj <89222581+Harshsfd@users.noreply.github.com>
2025-09-05 16:37:42 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,7 +3,5 @@ import { getProfile } from '../controllers/userController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
-
 router.get('/profile', authMiddleware, getProfile);
-
 export default router;
